6886568b6e4382145295bbaf345bc43992a526b5
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2023-10-05 20:11:38 +0530

    Update config.py
    Signed-off-by: Payal8800 <124197031+Payal8800@users.noreply.github.com>

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://te.legra.ph/file/b6567a3191ad32d4f40a4.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/2cda7bd6207b23d51830d.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/b6567a3191ad32d4f40a4.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
